bb91a33cdaefdfd5f6b2df05e78d2d85aff47aab
Chiara Caruso <carusochiara08@gmail.com>
2021-11-08 20:55:56 +0100

    problemi con setInterval

--- js/app.js
+++ js/app.js
@@ -1,15 +1,59 @@
 import {solarSystem} from './solarSystem.js';
+import {monkeysEquipe} from './monkeys.js';
 
 document.addEventListener("DOMContentLoaded", () => {
+    const modalOne = document.querySelector(".modaleOne-cont");
+    const modalTwo = document.querySelector(".modaleTwo-cont");
+    const btnAccept = document.querySelector(".accept");
+    const btnEvent = document.querySelector(".btn-event");
+    const page = document.querySelector(".main");
+    const monkeysCont = document.querySelector(".monkeys");
+
+
+    
 /**
- *  Add modal to index.html
+ *  Add modal to index.html.
  */
-    const modal = document.querySelector(".modale-cont");
+    setTimeout(() => {modalOne.style.display = "block"}, 10000);
 
-    const timeModal = setTimeout(() => {
-        modal.style.display = "block";
-    }, 10000);
+    btnAccept.addEventListener("click", () =>{
+        modalOne.style.display = "none";
+    })
 
+
+/**
+ *  Function to calculate diameter of planets.
+ */ 
     const planetDiameter = solarSystem.map((element) => element.radius * 2);
     console.log(planetDiameter);
+
+
+    btnEvent.addEventListener("click", () =>{
+        page.className = "hidden";
+        setTimeout(() => {modalTwo.style.display = "block";}, 5000);
+        setTimeout(() => {modalTwo.style.display = "none"}, 10000); 
+
+        const newH1 = document.createElement("h1");
+        const newH2 = document.createElement("h2");
+
+        newH1.className = "name-Monkey";
+        newH2.className = "aka-Monkey";
+
+        monkeysCont.appendChild(newH1);
+        monkeysCont.appendChild(newH2);
+        
+        const monkeys = monkeysEquipe.forEach((item) => {
+            
+            newH1.textContent = item.name;
+            newH2.textContent = item.aka;
+
+            monkeysCont.style.display = "block";         
+                    
+
+        })
+        setInterval(monkeys, 3000, newH1, newH2);
+
+
+    })
+    
 })
