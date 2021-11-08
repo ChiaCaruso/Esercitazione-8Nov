e3953098b909862cc6840825eaa183c92fc36452
Chiara Caruso <carusochiara08@gmail.com>
2021-11-08 17:24:43 +0100

    Prima parte

---
+++ js/app.js
@@ -0,0 +1,13 @@
+import {solarSystem} from './solarSystem.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+/**
+ *  Add modal to index.html
+ */
+    const modal = document.querySelector(".modale-cont");
+
+    const timeModal = setTimeout(() => {
+        modal.style.display = "block";
+    }, 10000);
+})
---
+++ js/solarSystem.js
@@ -0,0 +1,11 @@
+export const solarSystem = [
+    { name: "Sun", radius: 696340, surface: "star" },
+    { name: "Mercury", radius: 2440, surface: "terrestrial" },
+    { name: "Venus", radius: 6052, surface: "terrestrial" },
+    { name: "Earth", radius: 6371, surface: "terrestrial" },
+    { name: "Mars", radius: 3389, surface: "terrestrial" },
+    { name: "Jupiter", radius: 69911, surface: "gas giant" },
+    { name: "Saturn", radius: 58232, surface: "gas giant" },
+    { name: "Uranus", radius: 25362, surface: "ice giant" },
+    { name: "Neptune", radius: 24622, surface: "ice giant" },
+  ];
