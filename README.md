7d8e4c127b27f4c28fd31ca6a18bd808ef7a4f50
ChiaCaruso <carusochiara08@gmail.com>
2022-03-11 15:12:32 +0100

    sistemato readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Esercitazione-8Nov
+# Esercitazione-8Nov-Modale
+
+1. Aprite la pagina HTML più carina che avete creato fin'ora e aggiungete una MODALE
+che appaia 10 secondi dopo aver caricato il sito. La modale presenta due bottoni, uno che la chiude e uno che non fa nulla.
+
+2. Sulla base dell'oggetto solarSystem visto a lezione, creare delle funzioni che:
+convertano il valore della chiave name in Italiano. Per esempio, Saturn diverrà Saturno.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-import {solarSystem} from './solarSystem.js';
-import {monkeysEquipe} from './monkeys.js';
+import { solarSystem } from './solarSystem.js';
+import { monkeysEquipe } from './monkeys.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     const modalOne = document.querySelector(".modaleOne-cont");
@@ -10,50 +10,109 @@ document.addEventListener("DOMContentLoaded", () => {
     const monkeysCont = document.querySelector(".monkeys");
 
 
-    
-/**
- *  Add modal to index.html.
- */
-    setTimeout(() => {modalOne.style.display = "block"}, 10000);
 
-    btnAccept.addEventListener("click", () =>{
+    /**
+     *  Add modal to index.html.
+     */
+    setTimeout(() => { modalOne.style.display = "block" }, 5000);
+
+    btnAccept.addEventListener("click", () => {
         modalOne.style.display = "none";
     })
 
+    /**
+     *  Functions to translate names of planets and calculate diameter of planets.
+     */
 
-/**
- *  Function to calculate diameter of planets.
- */ 
+    const italianName = (planet) => {
+        switch (planet.name) {
+            case 'Sun':
+                planet.name = 'Sole';
+                break;
+
+            case 'Mercury':
+                planet.name = 'Mercurio';
+                break;
+
+            case 'Venus':
+                planet.name = 'Venere';
+                break;
+
+            case 'Earth':
+                planet.name = 'Terra';
+                break;
+
+            case 'Mars':
+                planet.name = 'Marte';
+                break;
+
+            case 'Jupiter':
+                planet.name = 'Giove';
+                break;
+
+            case 'Saturn':
+                planet.name = 'Saturno';
+                break;
+
+            case 'Uranus':
+                planet.name = 'Uranio';
+                break;
+
+            case 'Neptune':
+                planet.name = 'Nettuno';
+                break;
+        }
+        return planet;
+    }
+
+    console.log(solarSystem.map(italianName));
+
     const planetDiameter = solarSystem.map((element) => element.radius * 2);
     console.log(planetDiameter);
 
+    /**
+     *  Monkeys!
+     */
 
-    btnEvent.addEventListener("click", () =>{
+    btnEvent.addEventListener("click", () => {
         page.className = "hidden";
-        setTimeout(() => {modalTwo.style.display = "block";}, 5000);
-        setTimeout(() => {modalTwo.style.display = "none"}, 10000); 
 
-        const newH1 = document.createElement("h1");
-        const newH2 = document.createElement("h2");
+        const intervalModal = setInterval(() => {
+            if (modalTwo.style.display === "none") {
+                modalTwo.style.display = "block";
+            } else {
+                modalTwo.style.display = "none";
+            }
+        }, 2000);
 
-        newH1.className = "name-Monkey";
-        newH2.className = "aka-Monkey";
+        // setTimeout(() => {modalTwo.style.display = "block";}, 5000);
+        // setTimeout(() => {modalTwo.style.display = "none"}, 10000); 
 
-        monkeysCont.appendChild(newH1);
-        monkeysCont.appendChild(newH2);
-        
-        const monkeys = monkeysEquipe.forEach((item) => {
-            
-            newH1.textContent = item.name;
-            newH2.textContent = item.aka;
 
-            monkeysCont.style.display = "block";         
-                    
 
-        })
-        setInterval(monkeys, 3000, newH1, newH2);
+        // const monkeys = monkeysEquipe.forEach((item) => {
+
+        //     clearInterval(intervalModal);
+
+        //     const newH1 = document.createElement("h1");
+        //     const newH2 = document.createElement("h2");
+
+        //     newH1.className = "name-Monkey";
+        //     newH2.className = "aka-Monkey";
+
+        //     monkeysCont.appendChild(newH1);
+        //     monkeysCont.appendChild(newH2);
+
+        //     newH1.textContent = item.name;
+        //     newH2.textContent = item.aka;
+
+        //     monkeysCont.style.display = "block";         
+
+
+        // })
+        // setInterval(monkeys, 3000);
 
 
     })
-    
+
 })
